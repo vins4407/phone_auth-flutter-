048e9fa1f831d450c658042b0d6b6c60d4c333d8
Vinayak Kesharwani <90825635+vins4407@users.noreply.github.com>
2023-01-09 17:56:48 +0530

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,62 +11,11 @@ class homeScreen extends StatefulWidget {
 }
 
 class _homeScreenState extends State<homeScreen> {
-  int _page = 0;
-  late PageController pageController; // for tabs animation
-  
-  @override
-  void initState() {
-    super.initState();
-    pageController = PageController();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    pageController.dispose();
-  }
-
-  void onPageChanged(int page) {
-    setState(() {
-      _page = page;
-    });
-  }
-
-  void navigationTapped(int page) {
-    //Animating Page
-    pageController.jumpToPage(page);
-  }
+ 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        controller: pageController,
-        onPageChanged: onPageChanged,
-       // children: homeScreenItems,
-      ),
-      bottomNavigationBar: CupertinoTabBar(
-        backgroundColor: Colors.greenAccent,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-              color: (_page == 0) ? primaryColor : secondaryColor,
-            ),
-            label: '',
-            backgroundColor: primaryColor,
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.add_circle,
-                color: (_page == 1 ) ? primaryColor : secondaryColor,
-              ),
-              label: '',
-              backgroundColor: primaryColor),
-        ],
-        onTap: navigationTapped,
-        currentIndex: _page,
-      ),
-    );
+    body:Center(child: Text("welcome Sign In successfull"));
   }
 }
